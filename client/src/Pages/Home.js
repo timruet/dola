ce98f4b824f77c707ace9dab43687ccd1b7aee37
timruet <tr.timruettinger@gmail.com>
2024-06-12 12:11:50 +0200

    domain add/remove UI changes

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,6 +1,9 @@
 import '../dist/output.css';
 import volcano from "../images/volcano-cropped.png"
 
+import { useState } from 'react';
+import { TrashIcon } from '@heroicons/react/16/solid'
+
 export default function Home() {
     // const [username, setUsername] = useState();
 
@@ -17,7 +20,7 @@ export default function Home() {
         <>
             <div className="h-[26em] w-full bg-cover bg-home">
                 <div className="h-full w-full backdrop-blur flex justify-evenly items-center">
-                    <Logo/>
+                    <Logo />
                     <DomainsSelect />
                 </div>
                 <div className="h-fit flex text-center justify-center flex-grow">
@@ -32,6 +35,20 @@ export default function Home() {
 
 
 function DomainsSelect() {
+    const [domains, setDomains] = useState(["Construction", "Eldercare"]);
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const input = event.target[0].value;
+        setDomains(domains.concat(input));
+    }
+
+    function handleRemove(event, domain) {
+        event.preventDefault();
+        const newDomains = domains.filter((item) => item !== domain);
+        setDomains(newDomains);
+    }
+
     return (
         <div className="relative w-fit h-fit max-w-md">
             <div className="relative bg-white shadow-md rounded-lg px-3 py-2 w-96">
@@ -46,41 +63,30 @@ function DomainsSelect() {
                                 d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z" />
                         </svg>
                     </div>
-                    <input
-                        className="w-full rounded-md bg-gray-200 text-gray-700 leading-tight focus:outline-none py-2 px-2"
-                        id="search" type="text" placeholder="Pick or edit a domain" />
+                    <form onSubmit={(event) => handleSubmit(event)} className="flex w-full">
+                        <input className="w-full rounded-md bg-gray-200 text-gray-700 leading-tight focus:outline-none py-2 px-2"
+                            id="search" type="text" placeholder="Pick or add a domain" />
+                        <button type="submit" className="ml-auto bg-gray-700 hover:bg-gray-500 text-white font-bold py-1 px-3 rounded">
+                            Add
+                        </button>
+                    </form>
                 </div>
-                <div className="h-[10rem] py-3 text-sm overflow-y-scroll">
-                    <a href="domain/construction" className="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
-                        <span className="bg-gray-400 h-2 w-2 m-2 rounded-full"></span>
-                        <div className="flex-grow font-medium px-2">Construction</div>
-                    </a>
-                    <a href="domain/eldercare" className="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
-                        <span className="bg-green-400 h-2 w-2 m-2 rounded-full"></span>
-                        <div className="flex-grow font-medium px-2">Eldercare</div>
-                    </a>
-                    <a href="domain/eldercare" className="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
-                        <span className="bg-green-400 h-2 w-2 m-2 rounded-full"></span>
-                        <div className="flex-grow font-medium px-2">Eldercare</div>
-                    </a>
-                    <a href="domain/eldercare" className="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
-                        <span className="bg-green-400 h-2 w-2 m-2 rounded-full"></span>
-                        <div className="flex-grow font-medium px-2">Eldercare</div>
-                    </a>
-                    <a href="domain/eldercare" className="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
-                        <span className="bg-green-400 h-2 w-2 m-2 rounded-full"></span>
-                        <div className="flex-grow font-medium px-2">Eldercare</div>
-                    </a>
-                    <a href="domain/eldercare" className="flex justify-start cursor-pointer text-gray-700 hover:text-blue-400 hover:bg-blue-100 rounded-md px-2 py-2 my-2">
-                        <span className="bg-green-400 h-2 w-2 m-2 rounded-full"></span>
-                        <div className="flex-grow font-medium px-2">Eldercare</div>
-                    </a>
+                <div className="h-[10rem] min-h-[10rem] py-3 text-sm overflow-y-auto">
+                    {domains.map(domain => (
+                        <div key={domain} className="flex py-2">
+                            <span className="bg-gray-400 h-2 w-2 m-2 rounded-full"></span>
+                            <a href={`domain/${domain}`.toLowerCase()} className="text-gray-700 hover:text-blue-400">
+                                <span className="relative top-[2px] w-fit font-medium px-2">{domain}</span>
+                            </a>
+                            <button type="button" onClick={(event) => handleRemove(event, domain)} className="top-[2px] ml-auto cursor-pointer text-gray-700 hover:text-blue-400 rounded-md px-1 py-1"><TrashIcon className="h-[16px] w-[16px]" /></button>
+                        </div>
+                    ))}
                 </div>
-                <div className="block bg-gray-200 text-sm text-right py-2 px-3 -mx-3 -mb-2 rounded-b-lg">
+                {/* <div className="block bg-gray-200 text-sm text-right py-2 px-3 -mx-3 -mb-2 rounded-b-lg">
                     <button className="bg-gray-700 hover:bg-gray-500 text-white font-bold py-2 px-4 rounded">
                         Edit
                     </button>
-                </div>
+                </div> */}
             </div>
         </div>
     )
