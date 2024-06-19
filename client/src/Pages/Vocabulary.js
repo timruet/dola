7c1d2a21bd375412c9301377a5fc5d5dced11e6a
timruet <tr.timruettinger@gmail.com>
2024-06-19 23:23:58 +0200

    final clean up

--- client/src/Pages/Vocabulary.js
+++ client/src/Pages/Vocabulary.js
@@ -51,28 +51,27 @@ function VocabTable() {
   if (vocabulary) {
     return (
       <>
-        <div className="relative justify-center flex p-4">
-          <table className="table-fixed bg-white second:rounded-tl-lg second:rounded-tr-lg last:rounded-bl-lg last:rounded-br-lg border-collapse">
+        <div className="relative w-screen justify-center flex p-4">
+          <table className="table-fixed w-full bg-white second:rounded-tl-lg second:rounded-tr-lg last:rounded-bl-lg last:rounded-br-lg border-collapse">
             <thead>
               <tr className="p-3 border-b-8 bg-gray-200">
                 <th className="bg-gray-50 rounded-lg">
                   <button className="text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2">
                     <a href="#" className="">Edit</a>
                   </button>
-                </th>
-                <th className="bg-gray-50 rounded-lg">
                   <button className="text-white bg-gray-800 hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-3 py-2 mr-2 mb-2">
                     <a href={`/quizz/${domain}`} onClick={handleClick} className="">Study</a>
                   </button>
                 </th>
                 <th className="bg-gray-50 rounded-lg"></th>
                 <th className="bg-gray-50 rounded-lg"></th>
+                <th className="bg-gray-50 rounded-lg"></th>
               </tr>
               <tr className="bg-gray-200">
                 <th className="py-3 rounded-tl-lg"></th>
-                {/* <th className="px-8 py-3">ID</th> */}
-                <th className="pr-20 pl-5 py-3 p-">GERMAN</th>
-                <th className="py-3 rounded-tr-lg">ENGLISH</th>
+                <th className="px-8 py-3">ID</th>
+                <th className="pr-20 pl-5 py-3 p-">ENGLISH</th>
+                <th className="py-3 rounded-tr-lg">GERMAN</th>
               </tr>
             </thead>
             <tbody>
